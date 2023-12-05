988d1e0fec50d4e4bf546b736b44ad589b471db8
Augusto Carrasco <augustocarrascotoro@gmail.com>
2023-12-05 00:12:28 +0000

    Seccond commit

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect, useContext } from "react";
 import "../../styles/home.css";
 import { Context } from "../store/appContext";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 
 export const Home = () => {
 	const [agendaName, setAgendaName] = useState([]);
 	const { actions } = useContext(Context);
+	const navigate = useNavigate();
+
 
 	useEffect(() => {
 	}, []);
@@ -14,23 +16,28 @@ export const Home = () => {
 	const goToAgendaList = () => {
 		actions.setAgenda(agendaName);
 		setAgendaName('');
+		navigate('/home')
 	}
 
 	const deleteAgenda = () => {
-		actions.deleteAgenda(agendaName);
+		const agendaToDelete = prompt('Enter Agenda Name/Slug to be deleted: ')
+		if (agendaToDelete === null) {
+			return;
+		}
+		actions.deleteAgenda(agendaToDelete);
 	}
 
 	return (<>
 		<div className="text-center mt-5 bg-dark d-flex flex-column justify-content-between align-items-center">
 			<h1 className='text-light'>Welcome to your contact app</h1>
 			<div className="d-flex flex-column justify-content-between align-items-center">
-				<label className='text-light m-3' htmlFor='agenda'>Enter agenda Name: <input type='text' id='agenda' onChange={(event) => { setAgendaName(event.target.value) }}></input></label>
-				<Link to="/home" className=" m-3">
-					<p className="btn btn-success fw-bold" onClick={goToAgendaList}>
+				<form className="d-flex flex-column justify-content-between align-items-center" onSubmit={goToAgendaList}>
+					<label className='text-light m-3' htmlFor='agenda'>Enter agenda Name: <input type='text' id='agenda' required minLength='1' onChange={(event) => { setAgendaName(event.target.value) }}></input></label>
+					<button type="submit" className="btn btn-success fw-bold">
 						Go to the list
-					</p>
-				</Link>
-				<button className="btn btn-danger" onClick={deleteAgenda}>Delete Full Agenda</button>
+					</button>
+				</form>
+				<button type='button' className="btn btn-danger" onClick={deleteAgenda}>Delete An Agenda</button>
 			</div>
 		</div>
 	</>);
